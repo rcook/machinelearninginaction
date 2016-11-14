420493188a3df6aa079c003b0adce5ca5b27995a
Richard Cook <rcook@rcook.org>
2016-11-14 08:44:02 -0800

    Example use of Diagrams

--- charts/src/Main.hs
+++ charts/src/Main.hs
@@ -1,5 +1,22 @@
-module Main where
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+module Main (main) where
+
+import Diagrams.Backend.SVG.CmdLine
+import Diagrams.Prelude
+
+hilbert 0 = mempty
+hilbert n = hilbert' (n-1) # reflectY <> vrule 1
+         <> hilbert  (n-1) <> hrule 1
+         <> hilbert  (n-1) <> vrule (-1)
+         <> hilbert' (n-1) # reflectX
+  where
+    hilbert' m = hilbert m # rotateBy (1/4)
+
+diagram :: Diagram B
+diagram = strokeT (hilbert 6) # lc silver
+                              # opacity 0.3
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = mainWith diagram
