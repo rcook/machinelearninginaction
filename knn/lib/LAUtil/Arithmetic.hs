97a1b801c826440679732b4d98042acca876012e
Richard Cook <rcook@rcook.org>
2016-11-15 08:23:58 -0800

    Use konst and remove constantVector, constantMatrix

--- knn/lib/LAUtil/Arithmetic.hs
+++ knn/lib/LAUtil/Arithmetic.hs
@@ -8,7 +8,7 @@ import           Numeric.LinearAlgebra
 import           LAUtil.Data
 
 sumColumns :: Matrix R -> Matrix R
-sumColumns m = constantMatrix 1.0 1 (rows m) <> m
+sumColumns m = konst 1.0 (1, rows m) <> m
 
 sumRows :: Matrix R -> Matrix R
-sumRows m = constantMatrix 1.0 1 (cols m) <> tr m
+sumRows m = konst 1.0 (1, cols m) <> tr m
--- knn/lib/LAUtil/Data.hs
+++ knn/lib/LAUtil/Data.hs
@@ -1,13 +1,1 @@
-module LAUtil.Data
-  ( constantMatrix
-  , constantVector
-  ) where
-
-import qualified Data.Vector.Storable as VS
-import           Numeric.LinearAlgebra
-
-constantVector :: Double -> Int -> Vector R
-constantVector value n = VS.replicate n value
-
-constantMatrix :: Double -> Int -> Int -> Matrix R
-constantMatrix value r c = matrix c (replicate (r * c) value)
+module LAUtil.Data () where
