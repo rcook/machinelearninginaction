3448ea08ce3bbf4261015dde5b65f70471b32a8c
Richard Cook <rcook@rcook.org>
2016-11-16 21:22:09 -0800

    Almost done

--- charts2/src/Main.hs
+++ charts2/src/Main.hs
@@ -2,7 +2,7 @@ module Main (main) where
 
 import           Control.Monad
 import qualified Data.Map as M
-import           Data.Vector.Storable as VS hiding (foldr, forM_)
+import           Data.Vector.Storable as VS hiding (foldr, forM_, map)
 import           Graphics.Rendering.Chart.Backend.Diagrams
 import           Graphics.Rendering.Chart.Easy hiding (Matrix, Vector)
 import           Numeric.LinearAlgebra
@@ -21,28 +21,32 @@ seriesLabelIds :: Vector Z
 seriesLabelIds = fromList [10, 10, 20, 20, 10, 30]
 
 seriesLabels :: M.Map Z String
-seriesLabels = M.fromList [(10, "ten"), (20, "twenty")]
+seriesLabels = M.fromList [(10, "ten"), (20, "twenty"), (30, "thirty")]
 
 partitionIndices :: [Z] -> M.Map Z [Int]
 partitionIndices xs = foldr f M.empty (zip [0..] xs)
     where f (i, x) m = M.alter (\mb -> case mb of Nothing -> Just [i]; Just is -> Just (i : is)) x m
 
-main :: IO ()
-main = do
+type Coordinate = (R, R)
+type CoordinateList = [Coordinate]
+type SeriesPlotSpec = (String, CoordinateList)
+
+seriesPlotSpecs :: Matrix R -> [SeriesPlotSpec]
+seriesPlotSpecs series =
     let columns = toColumns series
         column0 = columns !! 0
         column1 = columns !! 1
         labelIds = VS.toList seriesLabelIds
         ps = partitionIndices labelIds
-    forM_ (M.toList ps) $ \(labelId, indices) -> do
+    in (flip map) (M.toList ps) $ \(labelId, indices) ->
         let subseries = foldr f [] indices
                           where f i ps = let p = ((VS.!) column0 i, (VS.!) column1 i) in p : ps
-        print subseries
+            Just labelText = M.lookup labelId seriesLabels
+        in (labelText, subseries)
 
-v :: [(R, R)]
-v = [(1.0, 1.0), (10.0, 10.0)]
-
-main2 :: IO ()
-main2 = toFile def "example.svg" $ do
+main :: IO ()
+main = toFile def "example.svg" $ do
     layout_title .= "Amplitude Modulation"
-    plot (points "foo" v)
+    let specs = seriesPlotSpecs series
+    forM_ specs $ \(labelText, subseries) -> do
+        plot (points labelText subseries)
