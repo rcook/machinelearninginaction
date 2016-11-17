a785bdce7aea098b721a219a2a6e3219c917d83b
Richard Cook <rcook@rcook.org>
2016-11-16 21:35:20 -0800

    Refactor some more

--- charts2/src/Main.hs
+++ charts2/src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main (main) where
 
 import           Control.Monad
@@ -7,50 +9,45 @@ import           Graphics.Rendering.Chart.Backend.Diagrams
 import           Graphics.Rendering.Chart.Easy hiding (Matrix, Vector)
 import           Numeric.LinearAlgebra
 
+type RRPlot l = EC l (PlotPoints R R)
 type Coordinate = (R, R)
 type CoordinateList = [Coordinate]
 type SeriesPlotSpec = (String, CoordinateList)
 
-series :: Matrix R
-series = matrix 2
-  [ 1.0, 1.1
-  , 2.0, 2.1
-  , 3.0, 3.1
-  , 4.0, 4.1
-  , 5.0, 5.1
-  , 6.0, 6.1
-  ]
+data Input = Input
+  { _values :: Matrix R
+  , _labelIds :: Vector Z
+  , _labels :: M.Map Z String
+  } deriving Show
 
-seriesLabelIds :: Vector Z
-seriesLabelIds = fromList [10, 10, 20, 20, 10, 30]
-
-seriesLabels :: M.Map Z String
-seriesLabels = M.fromList [(10, "ten"), (20, "twenty"), (30, "thirty")]
+input :: Input
+input = Input
+    (matrix 2 [1.0, 1.1, 2.0, 2.1, 3.0, 3.1, 4.0, 4.1, 5.0, 5.1, 6.0, 6.1])
+    (fromList [10, 10, 20, 20, 10, 30])
+    (M.fromList [(10, "ten"), (20, "twenty"), (30, "thirty")])
 
 partitionIndices :: [Z] -> M.Map Z [Int]
 partitionIndices xs = foldr f M.empty (zip [0..] xs)
     where f (i, x) m = M.alter (\mb -> case mb of Nothing -> Just [i]; Just is -> Just (i : is)) x m
 
-seriesPlotSpecs :: Matrix R -> Vector Z -> M.Map Z String -> [SeriesPlotSpec]
-seriesPlotSpecs series seriesLabelIds seriesLabels =
-    let columns = toColumns series
+seriesPlotSpecs :: Input -> [SeriesPlotSpec]
+seriesPlotSpecs Input{..} =
+    let columns = toColumns _values
         column0 = columns !! 0
         column1 = columns !! 1
-        labelIds = VS.toList seriesLabelIds
+        labelIds = VS.toList _labelIds
         ps = partitionIndices labelIds
     in (flip map) (M.toList ps) $ \(labelId, indices) ->
         let subseries = foldr f [] indices
                           where f i ps = let p = ((VS.!) column0 i, (VS.!) column1 i) in p : ps
-            Just labelText = M.lookup labelId seriesLabels
+            Just labelText = M.lookup labelId _labels
         in (labelText, subseries)
 
-seriesPlots :: Matrix R -> Vector Z -> M.Map Z String -> [EC l (PlotPoints R R)]
-seriesPlots series seriesLabelIds seriesLabels = map
-    (uncurry points)
-    (seriesPlotSpecs series seriesLabelIds seriesLabels)
+seriesPlots :: Input -> [RRPlot l]
+seriesPlots input = map (uncurry points) (seriesPlotSpecs input)
 
 main :: IO ()
 main = toFile def "example.svg" $ do
     layout_title .= "Amplitude Modulation"
-    let ps = seriesPlots series seriesLabelIds seriesLabels
+    let ps = seriesPlots input
     mapM_ plot ps
