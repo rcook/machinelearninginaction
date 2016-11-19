ed660562840eb1b26cbea8c1caf6b72b2bf9eb47
Richard Cook <rcook@rcook.org>
2016-11-19 09:51:40 -0800

    Add test for argSort

---
+++ mlutil/spec/SortingSpec.hs
@@ -0,0 +1,17 @@
+module SortingSpec
+  ( main
+  , spec
+  ) where
+
+import qualified Data.Vector.Unboxed as VU
+import           MLUtil
+import           Test.Hspec
+
+spec :: Spec
+spec = do
+    describe "argSort" $ do
+        it "should return ordered indices" $
+            argSort (vector [40.0, 30.0, 10.0, 11.0]) `shouldBe` VU.fromList [2, 3, 1, 0]
+
+main :: IO ()
+main = hspec spec
