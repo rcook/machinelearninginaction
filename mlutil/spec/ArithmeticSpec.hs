7e6fdd9cdc7a97fc278eaf8437b4dca619bfb531
Richard Cook <rcook@rcook.org>
2016-11-19 01:06:39 -0800

    Test Folding module

--- mlutil/spec/ArithmeticSpec.hs
+++ mlutil/spec/ArithmeticSpec.hs
@@ -12,11 +12,11 @@ m = matrix 3 [1.0, 2.0, 3.0, 4.0, 5.0, 100.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0]
 spec :: Spec
 spec = do
     describe "sumColumns" $ do
-        it "sums columns" $ do
+        it "sums columns" $
             sumColumns m `shouldBe` matrix 3 [20.0, 24.0, 122.0]
 
     describe "sumRows" $ do
-        it "sums rows" $ do
+        it "sums rows" $
             sumRows m `shouldBe` matrix 4 [6.0, 109.0, 21.0, 30.0]
 
 main :: IO ()
---
+++ mlutil/spec/FoldingSpec.hs
@@ -0,0 +1,36 @@
+module FoldingSpec
+  ( main
+  , spec
+  ) where
+
+import           Control.Exception
+import           MLUtil
+import           Test.Hspec
+
+m :: Matrix R
+m = matrix 3 [1.0, 2.0, 3.0, 4.0, 5.0, 100.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0]
+
+spec :: Spec
+spec = do
+    describe "columnHead" $ do
+        it "reads column head 0" $
+            columnHead m 0 `shouldBe` 1.0
+        it "reads column head 1" $
+            columnHead m 1 `shouldBe` 2.0
+        it "reads column head 2" $
+            columnHead m 2 `shouldBe` 3.0
+        it "fails if out of bounds" $
+            evaluate (columnHead m 3) `shouldThrow` errorCall "matrix indexing out of range"
+
+    describe "foldColumn" $ do
+        it "folds column 0" $
+            foldColumn (+) 0.0 m 0 `shouldBe` 20.0
+        it "folds column 1" $
+            foldColumn (+) 0.0 m 1 `shouldBe` 24.0
+        it "folds column 2" $
+            foldColumn (+) 0.0 m 2 `shouldBe` 122.0
+        it "fails if out of bounds" $
+            evaluate (foldColumn (+) 0.0 m 3) `shouldThrow` errorCall "matrix indexing out of range"
+
+main :: IO ()
+main = hspec spec
