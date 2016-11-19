b158c7c1dc2ae87745da4c2ab8407e3ab9134d73
Richard Cook <rcook@rcook.org>
2016-11-19 10:38:41 -0800

    Clean up Main

--- mlalgs/src/Main.hs
+++ mlalgs/src/Main.hs
@@ -14,35 +14,6 @@ import           MLUtil
 dataPath :: FilePath
 dataPath = "../Ch02/datingTestSet.txt"
 
-demoClassify0 :: IO ()
-demoClassify0 = do
-    let group :: Matrix R
-        group = matrix 2
-          [ 1.0, 1.1
-          , 1.0, 1.0
-          , 0.0, 0.0
-          , 0.0, 0.1
-          ]
-        labels :: M.Map LabelId String
-        labels = M.fromList [(1, "A"), (2, "B")]
-        labelIds :: VU.Vector LabelId
-        labelIds = VU.fromList [1, 1, 2, 2]
-
-    let r = classify0 (row [0.0, 0.0]) group labelIds 3
-        Just label = M.lookup r labels
-    print label
-
-    let r = classify0 (row [1.0, 1.2]) group labelIds 3
-        Just label = M.lookup r labels
-    print label
-
-input :: LabelledMatrix
-input = LabelledMatrix
-    (matrix 2 [1.0, 1.1, 2.0, 2.1, 3.0, 3.1, 4.0, 4.1, 5.0, 5.1, 6.0, 6.1])
-    (VU.fromList [10, 10, 20, 20, 10, 30])
-    (M.fromList [("ten", 10), ("twenty", 20), ("thirty", 30)])
-    (M.fromList [(10, "ten"), (20, "twenty"), (30, "thirty")])
-
 renderChapter2Figures :: IO ()
 renderChapter2Figures = do
     Just m <- readLabelledMatrix dataPath
@@ -53,9 +24,7 @@ renderChapter2Figures = do
     -- Figure 2.5
     renderSVG "Video games vs. frequent flyer miles" "figure-2.5.svg" (plots m 0 1)
 
-testNormalizeMatrixColumns :: IO ()
-testNormalizeMatrixColumns = do
-    Just m <- readLabelledMatrix dataPath
+    -- Normalized version of figure 2.5
     let MatrixNormalization{..} = normalizeMatrixColumns (lmValues m)
         m' = m { lmValues = mnValues }
     renderSVG "Normalized: video games vs. frequent flyer miles" "figure-2.5-normalized.svg" (plots m' 0 1)
@@ -87,7 +56,5 @@ datingClassTest = do
 
 main :: IO ()
 main = do
-    --demoClassify0
     --renderChapter2Figures
-    --testNormalizeMatrixColumns
     datingClassTest
