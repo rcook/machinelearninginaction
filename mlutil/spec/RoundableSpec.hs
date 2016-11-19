4ddb85019bae094aeccc0e50b51e67df2f633a8a
Richard Cook <rcook@rcook.org>
2016-11-19 09:42:25 -0800

    Round vector and matrix

--- mlutil/spec/RoundableSpec.hs
+++ mlutil/spec/RoundableSpec.hs
@@ -16,5 +16,13 @@ spec = do
         it "should round 0.123454 to 0.12345" $
             roundToPrecision 5 0.123454 `shouldBe` (0.12345 :: R)
 
+        it "should round [0.123456, 0.123455, 0.123454] correctly" $
+            roundToPrecision 5 (vector [0.123456, 0.123455, 0.123454])
+                `shouldBe` vector [0.12346, 0.12346, 0.12345]
+
+        it "should round [[0.123456, 0.123455], [0.123454, 0.123453]] correctly" $
+            roundToPrecision 5 (matrix 2 [0.123456, 0.123455, 0.123454, 0.123453])
+                `shouldBe` matrix 2 [0.12346, 0.12346, 0.12345, 0.12345]
+
 main :: IO ()
 main = hspec spec
