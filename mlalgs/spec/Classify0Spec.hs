8b24ce874ff3972b22289084956583ae8e342938
Richard Cook <rcook@rcook.org>
2016-11-19 10:34:15 -0800

    Add tests for classify0

--- mlalgs/spec/Classify0Spec.hs
+++ mlalgs/spec/Classify0Spec.hs
@@ -3,13 +3,40 @@ module Classify0Spec
   , spec
   ) where
 
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Map as M
+import           MLAlgs.Classify0
+import           MLUtil
 import           Test.Hspec
+
+dataSet :: Matrix R
+dataSet = matrix 2
+  [ 1.0, 1.1
+  , 1.0, 1.0
+  , 0.0, 0.0
+  , 0.0, 0.1
+  ]
+
+labelIds :: VU.Vector LabelId
+labelIds = VU.fromList
+  [ 1
+  , 1
+  , 2
+  , 2
+  ]
+
+labels :: M.Map LabelId String
+labels = M.fromList [(1, "A"), (2, "B")]
+
+k :: Int
+k = 3
 
 spec :: Spec
 spec = do
-    describe "roundToPrecision" $ do
-        it "should round 0.123456 to 0.12346" $
-            0.0 `shouldBe` 1.0
+    describe "classify0" $ do
+        it "should correctly classify" $ do
+            classify0 (row [0.0, 0.0]) dataSet labelIds k `shouldBe` 2
+            classify0 (row [1.0, 1.2]) dataSet labelIds k `shouldBe` 1
 
 main :: IO ()
 main = hspec spec
