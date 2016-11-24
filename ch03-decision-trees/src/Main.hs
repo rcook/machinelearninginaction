bb6e8e12b29a56cd2253416194da858c20abbbcd
Richard Cook <rcook@rcook.org>
2016-11-23 19:29:22 -0800

    Accumulate count of labels

--- ch03-decision-trees/src/Main.hs
+++ ch03-decision-trees/src/Main.hs
@@ -1,10 +1,33 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main (main) where
 
 import           Ch03DecisionTrees.Foo
+import qualified Data.Map as M
 import           DataFiles
+
+type Record = (Int, Int, String)
+
+dataSet :: [Record]
+dataSet =
+    [ (1, 1, "yes")
+    , (1, 1, "yes")
+    , (1, 0, "no")
+    , (0, 1, "no")
+    , (0, 1, "no")
+    ]
+
+labels :: [String]
+labels = ["no surfacing", "flippers"]
+
+-- cf trees.calcShannonEnt
+calculateShannonEntropy :: [Record] -> M.Map String Int
+calculateShannonEntropy rs =
+    let count = length rs
+    in foldr (\(_, _, label) m -> M.alter (\case { Nothing -> Just 1; Just n -> Just $ n + 1 }) label m) M.empty rs
 
 main :: IO ()
 main = do
-    x <- getDataFileName "x"
-    foo
+    let e = calculateShannonEntropy dataSet
+    print e
     putStrLn "Done"
